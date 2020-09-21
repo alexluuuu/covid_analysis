d583d1f1b94ba2a2d267174e322ebe7df7c55f10
Alex Lu <aeksanthr@gmail.com>
2020-09-20 20:45:19 -0400

    some changes to reflect cluster environment; couldn't get xls situation to run

--- test_cluster_script.py
+++ test_cluster_script.py
@@ -17,11 +17,15 @@ def main():
 			for prot in ['test_ref', 'test_Spike']:
 				allele_str = ",".join(alleles[pos:pos+size])
 				cluster_call_base = "sbatch -c 2 -t 4:0:0 -p shared --mail-type=begin --mail-type=end --mail-user=alu27@jhu.edu "
-				cluster_call_base += "--job-name=netmhc_%s_%d "%(prot, i)
+				cluster_call_base += " --job-name=netmhc_%s_%d "%(prot, i)
+                                cluster_call_base += " --output=output/%s_%d.o "%(prot, i)
+                                cluster_call_base += " --error=output/error/%j.e "
+
 				cluster_call_base += path_to_netmhc
 				cluster_call_base += " -l 8,9,10,11,12 -s " 
 				cluster_call_base += " -a %s "%(allele_str)
-				cluster_call_base += " %s.fasta > output/%s_%d.out "%(prot, prot, i)
+                                # cluster_call_base += "-xls 1 -xlsfile output/%s_%d.xls "%(prot, i)
+                                cluster_call_base += " data/%s.fasta"%(prot)
 				print(cluster_call_base)
 				# os.system(cluster_call_base)
 
